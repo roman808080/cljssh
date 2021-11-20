02e897c22246c8467573b0134c44911108fe1ab1
Roman Herasymenko <roman.herasymenko@gmail.com>
2021-11-20 21:20:04 +0100

    Added add-password, login

--- src/cljssh/core.clj
+++ src/cljssh/core.clj
@@ -11,6 +11,21 @@
                                             (.verify 100)
                                             (.getSession)))
 
+(defn add-password [session password]
+  (doto session
+    (.addPasswordIdentity password)))
+
+(defn login [session]
+  (-> session
+      (.auth)
+      (.verify 1000)))
+
+;; (defn login-with)
+
 (comment (start-client))
 (comment (create-session "roman" "localhost" 22))
+(comment (-> (create-session "roman" "localhost" 22)
+             (add-password "")
+             (login)))
+
 (comment (stop-client))
