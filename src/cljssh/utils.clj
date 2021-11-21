915ddd22de09966d18009babf5998dc2eb5fee3b
Roman Herasymenko <roman.herasymenko@gmail.com>
2021-11-21 21:59:47 +0100

    Added load-edn, refactored.

--- src/cljssh/utils.clj
+++ src/cljssh/utils.clj
@@ -1,14 +1,27 @@
 (ns cljssh.utils
-  (:require [clojure.java.io])
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [clojure.walk :as walk])
   (:import [java.util Properties]))
 
 (defn load-properties [file-name]
-  (with-open [^java.io.Reader reader (clojure.java.io/reader file-name)]
+  (with-open [reader (io/reader file-name)]
     (let [properties (Properties.)]
       (.load properties reader)
-      (into {} properties))))
+      (-> (into {} properties)
+          (walk/keywordize-keys)))))
 
-;; (defn convert-port-to-number [properties] (assoc 
-;; ))
+(defn load-edn
+  "Load edn from an io/reader source (filename or io/resource)."
+  [source]
+  (try
+    (with-open [reader (io/reader source)]
+      (edn/read (java.io.PushbackReader. reader)))
+
+    (catch java.io.IOException e
+      (printf "Couldn't open '%s': %s\n" source (.getMessage e)))
+    (catch RuntimeException e
+      (printf "Error parsing edn file '%s': %s\n" source (.getMessage e)))))
 
 (comment (load-properties "test.properties"))
+(comment (load-edn "properties.clj"))
