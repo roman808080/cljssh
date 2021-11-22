f8558802db30478042ac63d457c6454682d89953
Roman Herasymenko <roman.herasymenko@gmail.com>
2021-11-22 23:49:59 +0100

    Fixed an error with string type.

--- src/cljssh/utils.clj
+++ src/cljssh/utils.clj
@@ -6,7 +6,9 @@
 
 (defn convert-to-int [maybe-int]
   (try
-    (Integer/parseInt maybe-int)
+    (if (string? maybe-int)
+     (Integer/parseInt maybe-int)
+      maybe-int)
     (catch java.lang.NumberFormatException error nil)))
 
 (defn convert-to-int-if [maybe-int]
@@ -37,3 +39,20 @@
 (comment (load-edn "properties.clj"))
 (comment (convert-to-int "3.4"))
 (comment (convert-to-int-if "34"))
+
+(defn returns-itself [something]
+  something)
+
+(comment (returns-itself 3))
+(comment (into {} (map
+                   (fn [[k v]] {k (convert-to-int-if v)})
+                   {:a 1 :b "2"})))
+
+(comment (into {} [{:something 3}, {:another 4}]))
+(comment (convert-to-int-if 4))
+(comment (int? 4))
+(comment (int? 4.3))
+(comment (string? 4.3))
+(comment (string? "something"))
+(comment (if (string? "something") 4 5))
+(comment (if (string? 4) 4 5))
