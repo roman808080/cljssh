aef321c81c631bc33ebb6109bf8b2572cded9b60
Roman Herasymenko <roman.herasymenko@gmail.com>
2021-11-23 00:01:25 +0100

    Added convert-properties-to-int.

--- src/cljssh/utils.clj
+++ src/cljssh/utils.clj
@@ -7,21 +7,27 @@
 (defn convert-to-int [maybe-int]
   (try
     (if (string? maybe-int)
-     (Integer/parseInt maybe-int)
+      (Integer/parseInt maybe-int)
       maybe-int)
-    (catch java.lang.NumberFormatException error nil)))
+    (catch java.lang.NumberFormatException _ nil)))
 
 (defn convert-to-int-if [maybe-int]
   (if-let [converted-int (convert-to-int maybe-int)]
     converted-int
     maybe-int))
 
+(defn convert-properties-to-int [properties]
+  (into {} (map
+            (fn [[k v]] {k (convert-to-int-if v)})
+            properties)))
+
 (defn load-properties [file-name]
   (with-open [reader (io/reader file-name)]
     (let [properties (Properties.)]
       (.load properties reader)
       (-> (into {} properties)
-          (walk/keywordize-keys)))))
+          (walk/keywordize-keys)
+          (convert-properties-to-int)))))
 
 (defn load-edn
   "Load edn from an io/reader source (filename or io/resource)."
@@ -37,22 +43,3 @@
 
 (comment (load-properties "test.properties"))
 (comment (load-edn "properties.clj"))
-(comment (convert-to-int "3.4"))
-(comment (convert-to-int-if "34"))
-
-(defn returns-itself [something]
-  something)
-
-(comment (returns-itself 3))
-(comment (into {} (map
-                   (fn [[k v]] {k (convert-to-int-if v)})
-                   {:a 1 :b "2"})))
-
-(comment (into {} [{:something 3}, {:another 4}]))
-(comment (convert-to-int-if 4))
-(comment (int? 4))
-(comment (int? 4.3))
-(comment (string? 4.3))
-(comment (string? "something"))
-(comment (if (string? "something") 4 5))
-(comment (if (string? 4) 4 5))
