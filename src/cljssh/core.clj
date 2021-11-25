5b2346d76324d1b36e23fb28801c70035758d1aa
Roman Herasymenko <roman.herasymenko@gmail.com>
2021-11-25 23:08:24 +0100

    Partially implemented execute-command.

--- src/cljssh/core.clj
+++ src/cljssh/core.clj
@@ -1,6 +1,8 @@
 (ns cljssh.core
   (:require [cljssh.utils :as utils])
-  (:import [org.apache.sshd.client SshClient]))
+  (:import [org.apache.sshd.client SshClient]
+           [org.apache.sshd.common.channel Channel]
+           [java.io ByteArrayOutputStream]))
 
 (def property-file ".temp/properties.clj")
 
@@ -8,9 +10,9 @@
 (defn stop-client [client] (.stop client))
 
 (defn create-session [client user host port] (-> client
-                                            (.connect user host port)
-                                            (.verify 100)
-                                            (.getSession)))
+                                                 (.connect user host port)
+                                                 (.verify 100)
+                                                 (.getSession)))
 
 (defn add-password [session password]
   (doto session
@@ -21,16 +23,29 @@
       (.auth)
       (.verify 1000)))
 
+(defn execute-command [session command]
+  (with-open [response-stream (ByteArrayOutputStream.)
+              error-stream (ByteArrayOutputStream.)
+              channel (.createChannel session Channel/CHANNEL_SHELL command)]
+    (doto channel
+      (.setOut response-stream)
+      (.setErr error-stream)
+      )
+    (-> channel
+        (.open)
+        (.verify 1000))))
+
 (defn operate-on-connection [{:keys [host port user password]}]
   (let [client (. SshClient setUpDefaultClient)]
     (start-client client)
 
-     (with-open [session (create-session client user host port)]
-       (-> session
-           (add-password password)
-           (login)))
+    (with-open [session (create-session client user host port)]
+      (-> session
+          (add-password password)
+          (login))
+      (execute-command session "dir"))
 
-     (stop-client client)))
+    (stop-client client)))
 
 (defn -main []
   (-> (utils/load-edn property-file)
