20fedfcfe341a1dece6d8c08afcb63cc13479686
Roman Herasymenko <roman.herasymenko@gmail.com>
2021-12-15 20:45:09 +0100

    Nothing useful. Playing with arrays.

--- src/cljssh/utils.clj
+++ src/cljssh/utils.clj
@@ -17,8 +17,11 @@
       (printf "Error parsing edn file '%s': %s\n" source (.getMessage e))
       {})))
 
+(defn get-str-array [& strings-to-convert]
+  (to-array strings-to-convert))
+
 (defn path-join [p & ps]
-  (str (.normalize (java.nio.file.Paths/get p (into-array String ps)))))
+  (str (.normalize (java.nio.file.Paths/get p (get-str-array ps)))))
 
 (defn get-path-object [str-path]
   (Path/of str-path (into-array String [])))
@@ -27,4 +30,33 @@
 (comment (Paths/get "my/complicated/path" (into-array String ["hell"])))
 (comment (path-join "my/complicated/path" "hell"))
 (comment (get-path-object "/path/to/my/strange/path"))
+(comment (into-array String [nil]))
 (comment (into-array String []))
+(comment (get-str-array "hell"))
+(comment (get-str-array))
+(comment (type (get-str-array)))
+
+(comment (class (to-array '(0))))
+(comment (make-array String 4))
+(comment (cast Number 1))
+
+;; (defn print-array [array-to-print]
+  ;; (amap))
+
+(comment (into-array String ""))
+(comment (into-array Number [1 2 3]))
+(comment (into-array Number nil))
+(comment (into-array Number '()))
+(comment (into-array Number '(1 2 3 4)))
+
+(def array-value (into-array Number '(1 2 3 4)))
+(comment (first array-value))
+(comment (second array-value))
+
+(def second-array (amap array-value
+                        idx
+                        ret
+                        (+ (int 1)
+                           (aget array-value idx))))
+
+(comment (aget second-array 0))
