630ece830b441f41161d87340aefcbdfe1e80d89
Roman Herasymenko <roman.herasymenko@gmail.com>
2021-12-25 21:16:56 +0100

    Added functions for starting, stopping and creating the server.

--- src/cljssh/server.clj
+++ src/cljssh/server.clj
@@ -3,12 +3,18 @@
   (:import [org.apache.sshd.server SshServer]
            [org.apache.sshd.server.keyprovider SimpleGeneratorHostKeyProvider]))
 
-(defn run-server [port]
-  (doto (SshServer/setUpDefaultServer)
+(defn create-default-server []
+  (SshServer/setUpDefaultServer))
+
+(defn run-server [server port]
+  (doto server
     (.setPort port)
     (.setKeyPairProvider (SimpleGeneratorHostKeyProvider. (utils/path-object "key.ser")))
     (.start)))
 
-(comment (run-server 2222))
-(comment (SimpleGeneratorHostKeyProvider. (utils/path-object "key.ser")))
-(comment (utils/path-object "key.ser"))
+(defn stop-server [server]
+  (.stop server))
+
+(comment (def server (create-default-server)))
+(comment (run-server server 2222))
+(comment (stop-server server))
